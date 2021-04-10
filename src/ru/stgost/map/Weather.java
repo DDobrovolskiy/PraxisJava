8919fd6a1042dfd909d79ee3db18ac21ca2d000e
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-10 10:21:13 +0500

    praxis

---
+++ src/ru/stgost/map/Weather.java
@@ -0,0 +1,56 @@
+package ru.stgost.map;
+
+import java.util.*;
+
+public class Weather {
+    public static List<Info> editData(List<Info> list) {
+        List<Info> rsl = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        list.stream().forEach(info -> {
+            if (map.containsKey(info.city)) {
+                map.put(info.city, map.get(info.city) + info.rainfall);
+            } else {
+                map.put(info.city, info.rainfall);
+            }
+        });
+        map.forEach((s,i) -> rsl.add(new Info(s,i)));
+        return rsl;
+    }
+
+    public static class Info {
+        private String city;
+
+        private int rainfall;
+
+        public Info(String city, int rainfall) {
+            this.city = city;
+            this.rainfall = rainfall;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public int getRainfall() {
+            return rainfall;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Info info = (Info) o;
+            return rainfall == info.rainfall &&
+                    Objects.equals(city, info.city);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(city, rainfall);
+        }
+    }
+}
